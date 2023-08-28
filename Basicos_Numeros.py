7b2b10b8b36286dab9ee8a5bb135e19f2269c4b6
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-27 19:19:48 -0600

    +,-,*,/,^

---
+++ Basicos_Numeros.py
@@ -0,0 +1,14 @@
+#operaciones basicas 
+
+suma = 5+6
+resta= 7-4
+mult =  6*6
+div = 45/5 
+#imprimir varios valores de dif variables 
+#dentro del print si utilizamos algun operador nos lo va a realizar siempre y cuando tengamos numeros
+print (suma, resta, mult, div )
+print (suma + resta + mult + div )
+
+#calcular exponentes 
+elevado = 2 ** 3
+print(elevado) 
