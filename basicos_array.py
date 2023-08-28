bf4d6a8ed9a3bd576a6968c6d2fc1127be8f3507
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-27 19:31:49 -0600

    lista

---
+++ basicos_array.py
@@ -0,0 +1,6 @@
+#creacion de la lista
+lista= ["cpu", "ram", "disco"]
+# de esta forma se presenta la representaicon de la lista 
+print (lista)
+#podemos obtener solo un valor de la lista se empieza a contar desde el 0
+print (lista[2])
