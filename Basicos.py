96c170226391b4450d40f35b737a4091d400b691
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-27 18:43:32 -0600

    variable,suma, mult, string

--- Basicos.py
+++ Basicos.py
@@ -1,6 +1,22 @@
+#!/usr/bin/env python
+#-*- coding: cp1252
+#-*- coding: 850 _*_
+#-*- coding: utf-8 -*- 
+
+#usar acentos las lineas anteriores 
+
+
 print ("hello world")
-r= "soy omar"
+r= "soy omar" #creacion de variable con string
 print (r)
 s=5+4;
 m=s*23
 print(m)
+
+# se peude cambiar entre tipos de valores sin tener que declararlo 
+m= 'hola' #de esta forma se declara un string o con doble comilla ""
+print(m)
+#concatenar strings
+#se puede de esta manera o agregando variables 
+Z= m+ ' Limón yo'+ " " + r
+print(Z)
