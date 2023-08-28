adbcdc7be8911adbb325bc9def2cd9bd6428afbf
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-27 19:10:07 -0600

    var,Strings,completo

---
+++ Basicos_String.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+#usar acentos las lineas anteriores  uutf 8
+
+print ("hello world")
+r= "soy omar" #creacion de variable con string
+print (r)
+
+# se peude cambiar entre tipos de valores sin tener que declararlo 
+r= 'hola' #de esta forma se declara un string o con doble comilla ""
+print(m)
+#concatenar strings
+#se puede de esta manera o agregando variables 
+Z= m+ ' Limón yo'+ " " + r
+print(Z)
+
+#metodos para los strings 
+# letra mayuscula para una string 
+t= "hola soy omar"
+t = t.title() #nos cambia el valor del string a cada palabra la primera letra en mays
+print (t)    
+#poner en mayusculas 
+t = t.upper() #nos cambia el valor del string  mays
+print (t)   
+#poner en minusculas
+t = t.lower() #nos cambia el valor del string  min
+print (t) 
+print ("")
+#uso de saltos de linea y tabulacion 
+#salto de linea \n
+#tabulacion \t
+#los comandos deben de ir dentro de las comillas 
+print('Stings salto y tab\n \tCómo estas? ')
