f694ddcc10d1e64b8b4972ed439a9442f6b61a1f
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-30 12:04:20 -0600

    funciones

---
+++ Funciones.py
@@ -0,0 +1,16 @@
+
+####Creacion de una funcion####
+print("\n######funciones########")
+def saludar():
+    print("Hola buenas tardes ")
+
+saludar()
+
+print("\n######mandar valores########")
+def familia(nombre):
+    print(nombre + " Simpson")
+ 
+familia("Homero")
+familia("Lisa")
+familia("Bart")
+familia("Marge")
