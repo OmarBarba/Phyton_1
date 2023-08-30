2fc061fb8de806a22935da05428be752f9fa9e0e
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-30 00:17:14 -0600

    for, continue, pass

--- Bucle_for.py
+++ Bucle_for.py
@@ -1,5 +1,15 @@
 
+cursos = ["c", "c+", "c#"]
 
 for x in "Python":
     print(x)
     
+for s in cursos: 
+    if s == "c#":
+        continue # lo salta 
+    print(s)
+
+#bucle for basio "pass"
+ 
+for d in []:
+    pass
