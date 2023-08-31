89213870b98a25646a59221f9c27ab1e81fc3a08
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-08-30 18:32:19 -0600

    arb, clase,

--- Funciones.py
+++ Funciones.py
@@ -14,3 +14,23 @@ familia("Homero")
 familia("Lisa")
 familia("Bart")
 familia("Marge")
+
+#####datos arbitrarios###
+
+
+def alumnos(*args):
+    print("EL ultimo alumno es: "+ args[2] +" y el primero es: "+ args[0]+ ".")
+
+
+alumnos("omar", "alex", "Javier")
+print("\n######clase mandando valores########")
+def alumnos_profesor (profesor, sustituto,*args):
+    print("profesor: " + profesor)
+    print("sustituto: " + sustituto)
+    for x in args: 
+        print("Alumno: " + x )
+
+lista_al = ["omar", "alex", "Javier"]
+alumnos_profesor("Antonio", "juan", *lista_al)
+
+    
