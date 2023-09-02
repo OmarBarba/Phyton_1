f5f12b4b95a7b9000c3b80aea7b8387fc2ad178d
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-09-02 10:09:40 -0600

    re_metacrater, try, except

--- 15_espreciones_regulares2.py
+++ 15_espreciones_regulares2.py
@@ -23,3 +23,39 @@ print(res3)
 ####buscar caracter por separado#########
 res2 = re.findall("[a]", texto)
 print(res2)
+
+##########################################
+#############EXEPCIONES###################
+#########################################
+
+################TRY######################
+################Except###################
+
+#probar errores 
+variable  = "correcto "
+try: 
+    print(variable)
+except:
+    print("hay un error")
+
+#esto no sirve para poder corroborar que lo que estamos haciendo tiene sentido
+# de igual forma mostraremos el uso de execpciones de forma practica 
+
+reinicio = True
+
+while reinicio: 
+    try:
+        num3 = int(input("Introduce un número para sumar"))
+        num4 = int(input("Introduce un número para sumar")) 
+    except ValueError:
+        print("ingresa un número sin caracter")
+    else:
+        print("El resultado es: ", num3 + num4)
+    finally:
+        pregunta = input("Quieres seguir sumando? introduce S/N \n ")
+        if  pregunta == "N":
+            reinicio = False
+        elif pregunta == "S": 
+            reinicio = True
+        else:
+            print("Ingresa 'N' para salir o 'S' para seguir")
