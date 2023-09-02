a15ece4f55d5555b72d875059738f371e623464b
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-09-02 00:40:59 -0600

    import, lib y lambda,fecha

--- 13_modulos_funciones_lambda.py
+++ 13_modulos_funciones_lambda.py
@@ -9,3 +9,12 @@ area(2)
 
 area = lambda radio : round(math.pi *radio*radio,2)
 print (area(2))
+
+#agregar fechas
+import datetime
+
+fecha = datetime.datetime.now()
+print(fecha)
+fecha = datetime.datetime(2020,9,29,10,50,45)
+print(fecha)
+print("año: ", fecha.year)
